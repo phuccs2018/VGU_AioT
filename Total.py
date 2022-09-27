63d15c7ee61fb942ac5bf7832c0538f0f3b89154
Nghia Le <66348769+meohoangdeptrai1407@users.noreply.github.com>
2022-09-27 14:13:21 +0700

    Update Total.py

--- Total.py
+++ Total.py
@@ -5,9 +5,11 @@ import random
 import time
 import serial.tools.list_ports
 
+
+
 AIO_FEED_ID = ["humidity", "button", "humidity-graph","tem","tem-graph"] # tên feed trong dashboard
-AIO_USERNAME = "FBIWarning" # tên Dashboard
-AIO_KEY = "aio_mLYz81Ob7eCVtu9NJsojnHlJTu2t" # Mã key
+AIO_USERNAME = "FBIWarning" # tên tài khoản của bạn
+AIO_KEY = "aio_mLYz81Ob7eCVtu9NJsojnHlJTu2t" # Mã key của dashboard
 TIME = 2
 
 #Hàm Kết nối
@@ -19,6 +21,7 @@ def connected(client):
 
 #Hàm Thông báo
 def subscribe(client, userdata, mid, granted_qos):
+    // Biến mid sẽ cho giá trị từ 1 đến index cuối của cái list AIO_FEED_ID
     print(f"Subcribe {AIO_FEED_ID[mid-1]} thành công ... ")
 
 #Ngắt kết nối
@@ -26,11 +29,13 @@ def disconnected(client):
     print("Ngắt kết nối ...")
     sys.exit(1)
 
-#Những hàm thông báo sẽ được gửi tin nhắn
+#Những hàm thông báo sẽ được gửi tin nhắn 
+# payload: hàm dùng để nhận giá trị từ adafruit.io
+# feed_id: tên của feed tương ứng
 def message(client, feed_id, payload):
     print(f"Nhận dữ liệu : {payload} từ {feed_id}")
 
-
+# Chạy chương trình
 client = MQTTClient(AIO_USERNAME, AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
@@ -38,6 +43,8 @@ client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
+
+#Tạo hàm loop để đảm bảo chương trình vẫn đang hoạt động
 while True:
     pass
 
